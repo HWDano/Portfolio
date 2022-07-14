31cf21b4e252bd6e54a212a3256a76784e3ca556
HW_Dano <DASierraH@outlook.com>
2022-07-14 13:40:59 -0500

    Temporary database 2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ my_email = os.environ.get('MY_EMAIL')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('sqlite:///projects_database.db', 'DATABASE_URL')  #
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects_database.db'  # os.environ.get('DATABASE_URL', 'sqlite:///projects_database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
